995813427112b6f387b17506bb1d6139db63a45e
work-141 <fzttest1@163.com>
2018-07-30 20:03:23 +0800

    update html css

--- registry/dockerweb.py
+++ registry/dockerweb.py
@@ -2,7 +2,7 @@
 # @Author: fangzt <295157914@qq.com>
 # @Date:   2018-07-29 16:55:42
 # @Last Modified by:   fzt
-# @Last Modified time: 2018-07-30 13:44:08
+# @Last Modified time: 2018-07-30 19:07:12
 
 from flask import Flask, render_template, request
 
@@ -14,12 +14,20 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/registry/', methods=['GET', 'POST'])
+@app.route('/registry', methods=['GET', 'POST'])
 def registry():
-    if request.method == 'GET' :
+    if request.method == 'GET':
         return render_template('registry.html')
     else :
         pass
+
+@app.route('/setting', methods=['GET', 'POST'])
+def setting():
+    if request.method == 'GET':
+        return render_template('setting.html')
+    else :
+        pass
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5100)
